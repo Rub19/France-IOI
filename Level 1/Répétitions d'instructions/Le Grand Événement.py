c2665ae73f3b80d0f93752cbdd08814d18b3db4f
Rub19 <stormrub019.lsp@gmail.com>
2023-04-24 19:13:23 +0200

    new

---
+++ Level 1/Répétitions d'instructions/Le Grand Événement.py
@@ -0,0 +1,18 @@
+from robot import *
+
+for i in range(9):
+   haut()
+
+for j in range(4):
+   droite()
+   for i in range(8):
+       bas()
+   droite()
+   for i in range(8):
+      haut()     
+droite()
+for i in range(9):
+   bas()
+   
+for i in range(9):
+   gauche()
